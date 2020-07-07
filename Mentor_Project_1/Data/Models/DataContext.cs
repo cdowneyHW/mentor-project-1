8da2dba8ae4efaa63c3bbe65a67bb42e1b0b0bfd
Colton Downey <cdowney@healthwise.org>
2020-07-07 15:41:39 -0600

    first commit

---
+++ Mentor_Project_1/Data/Models/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Mentor_Project_1.Data.Models
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataContext(IConfiguration configuration, DbContextOptions<DataContext> options) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+ 
+
+        public DbSet<Blog> Blogs { get; set; }
+        /*
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        */
+
+ 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(_configuration.GetConnectionString("DbConnectionString"));
+    }
+}
