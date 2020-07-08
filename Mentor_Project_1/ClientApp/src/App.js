816c04fdb9bf8f6e10f916d12ef99cc599994601
cdowneyHW <cdowneyHW@users.noreply.github.com>
2020-07-08 14:00:26 -0600

    modified app.js

--- Mentor_Project_1/ClientApp/src/App.js
+++ Mentor_Project_1/ClientApp/src/App.js
@@ -13,11 +13,11 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/blog' component={Blog} />
+        <Route exact path='/' component={Home} /> 
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
       </Layout>
     );
   }
 }
+        //<Route path='/blog' component={Blog} />
