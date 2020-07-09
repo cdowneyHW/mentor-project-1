41dbcf953e2b902966662cb9d180a0918e6071a4
cdowneyHW <cdowneyHW@users.noreply.github.com>
2020-07-09 15:21:59 -0600

    added not default constructor

--- Mentor_Project_1/Data/DTOs/CreateBlogResponse.cs
+++ Mentor_Project_1/Data/DTOs/CreateBlogResponse.cs
@@ -7,6 +7,11 @@ namespace Mentor_Project_1.Data.DTOs
 {
     public class CreateBlogResponse
     {
+        public CreateBlogResponse(int blogID, string url) =>
+            (this.BlogID, this.Url) = (blogID, url);
+
+        public CreateBlogResponse() { }
+
         public int BlogID { get; set; }
         public string Url { get; set; }
     }
