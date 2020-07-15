547d157110e5af95b04de339af79c10a50df0aef
cdowneyHW <cdowneyHW@users.noreply.github.com>
2020-07-14 18:37:09 -0600

    added blog react component

--- Mentor_Project_1/ClientApp/src/components/Blog.js
+++ Mentor_Project_1/ClientApp/src/components/Blog.js
@@ -1,1 +1,40 @@
-﻿
+﻿import React, { Component } from 'react';
+
+export class Blog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { urlValue: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = event => {
+        this.setState({ urlValue: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        fetch('http://localhost:32545/api/blog', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                url: this.state.urlValue
+            })
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <h1>Blog</h1>
+                    <input type="text" value={this.state.urlValue} onChange={this.handleChange} />
+                    <button className="btn btn-primary" type="submit" value="Submit" />
+                </div>
+            </form>
+        );
+    }
+}
